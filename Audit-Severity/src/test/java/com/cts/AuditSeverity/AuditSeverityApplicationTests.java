cb28e1db74f7a10c08ff6b72dbacecfd9c2ae392
Zia-Sana <95747537+Zia-Sana@users.noreply.github.com>
2022-02-01 19:20:11 +0530

    Add files via upload

---
+++ Audit-Severity/src/test/java/com/cts/AuditSeverity/AuditSeverityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.AuditSeverity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditSeverityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
