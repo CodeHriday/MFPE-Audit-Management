bbd4b9ed27a6c061e97e0cddb1f9da13193de392
CodeHriday <74758879+CodeHriday@users.noreply.github.com>
2022-02-03 12:57:54 +0530

    working commit
    code is working. i have added comments and made the required changes

---
+++ authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Hriday', 'hriday7', 'hri1234', 'Project-1');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Menaka', 'menaka1', 'mena1234', 'Project-2');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Harsh', 'harsh10', 'har1234', 'Project-3');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Vaishnavi', 'vaish15', 'vai1234', 'Project-4');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Ruksana', 'ruksana11', 'ruk1234', 'Project-5');
---
+++ authorization/src/test/java/com/mfpe/pod2cohort23/authorization/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfpe.pod2cohort23.authorization.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
