488181318e67767e7feeab2b8cdf2f6ae2f44c2a
CodeHriday <74758879+CodeHriday@users.noreply.github.com>
2022-02-03 12:58:37 +0530

    Delete Audit-Authorization directory

--- Audit-Authorization/src/main/java/com/mfpe/pod2cohort23/AuditAuthorization/exception/ProjectUserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mfpe.pod2cohort23.AuditAuthorization.exception;
-
-public class ProjectUserNotFoundException extends Exception { 	//extends RuntimeException
-private static final long serialVersionUID = 1L;
-	
-	public ProjectUserNotFoundException(String message) {
-		super(message);
-	}
-}
--- Audit-Authorization/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user_login(id, name, username, password, project_name) VALUES (1, 'Hriday', 'hriday', 'hriday123', 'Project 1');
-INSERT INTO user_login(id, name, username, password, project_name) VALUES (2, 'Menaka', 'menaka', 'menaka123', 'Project 1');
-INSERT INTO user_login(id, name, username, password, project_name) VALUES (3, 'Harsh', 'harsh', 'harsh123', 'Project 1');
-INSERT INTO user_login(id, name, username, password, project_name) VALUES (4, 'Vaishnavi', 'vaishnavi', 'vaish123', 'Project 1');
-INSERT INTO user_login(id, name, username, password, project_name) VALUES (5, 'Ruksana', 'ruksana', 'ruksana123', 'Project 1');
