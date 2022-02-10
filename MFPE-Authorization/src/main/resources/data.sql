02f2111642974b32f58f7e9620edb35a0c9ca95b
CodeHriday <74758879+CodeHriday@users.noreply.github.com>
2022-02-10 14:55:47 +0530

    Delete MFPE-Authorization directory

--- MFPE-Authorization/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Hriday Ganga', 'hriday7', 'hri1234', 'Project-1');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'G Menaka', 'menaka1', 'mena1234', 'Project-2');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Harsh Goyal', 'harsh10', 'har1234', 'Project-3');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Vaishnavi Srivastava', 'vaishnavi', '1234', 'Project-4');
-INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Ruksana Abdul Malik', 'ruksana11', 'ruk1234', 'Project-5');
